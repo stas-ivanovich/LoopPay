7c46009d6960c66b2fb3e704d05e60a80516104b
stas-ivanovich <stativanov@outlook.com>
2025-05-20 10:29:46 +0000

    feat: add custom errors for gas optimization

---
+++ src/errors/Errors.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+error InvalidRecipient();
+error InvalidAmount();
+error LoopNotActive();
+error LoopAlreadyCancelled();
+error LoopNotPaused();
+error ExecutionTooEarly();
+error TransferFailed();
