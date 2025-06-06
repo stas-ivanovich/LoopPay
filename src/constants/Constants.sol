eafdacee65933f8baa4c502e91c68dcd0e3154a4
stas-ivanovich <stativanov@outlook.com>
2025-06-06 16:35:10 +0000

    chore: add constants for Base USDC addresses

---
+++ src/constants/Constants.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+library Constants {
+    uint256 constant SECONDS_PER_DAY = 1 days;
+    uint256 constant SECONDS_PER_WEEK = 7 days;
+    uint256 constant SECONDS_PER_MONTH = 30 days;
+
+    address constant BASE_USDC_MAINNET = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
+    address constant BASE_USDC_SEPOLIA = 0x036CbD53842c5426634e7929541eC2318f3dCF7e;
+}
